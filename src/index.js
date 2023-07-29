84e52dbd2a67cace26c36ff63b91d98279c41abd
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2023-07-29 15:29:49 +0530

    code deploy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import axios from "axios";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./authContext/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+axios.defaults.baseURL = "http://localhost:8800/api/";
 root.render(
   <BrowserRouter>
     <AuthContextProvider>
